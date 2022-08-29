a384add742dac298cbc06504fa23c3431c089b71
*Matheus <*thebravematteo33@protonmail.com*>
2022-08-29 14:09:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button"
+
+export function App() {
+    return  (
+      <>
+        <Button color="primary"/>
+        <Button color="secondary"/>
+        <Button color="success"/>
+        <Button color="danger"/>
+        <Button />
+        </>
+    )
+}
