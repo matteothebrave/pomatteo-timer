fd73aec9f2f5a73b6ce9ee42cd3ef227351f89aa
*Matheus <*thebravematteo33@protonmail.com*>
2022-09-05 14:14:02 -0300

    feat/Added routes and created pages to be rendered on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 // import { defaultDark } from "./styles/themes/defaultDark";
 export function App() {
   return (
     // Here is where you switch the theme from styles/themes/default.ts (DONT FORGET TO IMPORT)
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
