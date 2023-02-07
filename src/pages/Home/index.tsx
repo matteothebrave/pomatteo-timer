d6764960dfc8bfa3f101fae005330f4ca7718732
*Matheus <*thebravematteo33@protonmail.com*>
2023-02-07 10:34:10 -0300

    Feat/pomatteo-timer/setting a return to clear interval whenever a new cycle starts. before, it was using the time which passed, not reseting. Clear Interval is a JS common function. added also the timer to browser window. using a native function of JS document.title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -56,11 +56,18 @@ export function Home() {
   const activeCycle = cycles.find(cycle => cycle.id == activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      const interval = setInterval(() => {
         setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
       }, 1000)
     }
+
+    return () => {
+      clearInterval(interval)
+    }
+
   }, [activeCycle])
 
   function handleCreateNewCycle(data: NewCycleFormData) {
@@ -75,6 +82,8 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id)
+    setAmountSecondsPassed(0) // Reset to 0 whenever a new project is created.
+
     reset();   // return to the defaultValues  (integration used by zod. line 38)
   }
 
@@ -89,6 +98,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds])
 
   const task = watch('task')
   const isSubmitDisabled = !task
